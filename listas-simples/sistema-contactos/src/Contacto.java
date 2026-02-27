9de53a7adfb34122edf7708cff0ef5c6defd55fe
Jerson Garcia <jersonjavier@gmail.com>
2026-02-27 07:31:57 -0500

    se crea logica de lista simple

--- listas-simples/sistema-contactos/src/Contacto.java
+++ listas-simples/sistema-contactos/src/Contacto.java
@@ -9,6 +9,30 @@ public class Contacto {
         this.correo = correo;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
     @Override
     public String toString() {
         return "Contacto{" +
